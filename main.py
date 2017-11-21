d7e17c96164047b3a0f79eb75abf037f7abe1652
andrew <andrewliao11@gmail.com>
2017-11-21 15:08:40 -0800

    add save flags

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ from maml import MAML
 
 def argsparser():
     parser = argparse.ArgumentParser("Tensorflow Implementation of MAML")
+    # Dataset
     parser.add_argument('--dataset', help='environment ID', choices=['sin'],
                         required=True)
+    # MAML
     parser.add_argument('--K', type=int, default=10)
-    parser.add_argument('--max_steps', type=int, default=1e6)
     parser.add_argument('--model_type', type=str, default='fc')
     parser.add_argument('--loss_type', type=str, default='MSE')
+    # Train
+    parser.add_argument('--max_steps', type=int, default=7e4)
     parser.add_argument('--alpha', type=float, default=1e-3)
     parser.add_argument('--beta', type=float, default=1e-3)
     parser.add_argument('--batch_size', type=int, default=25)
+    # Test
+    parser.add_argument('--test_sample', type=int, default=100)
     args = parser.parse_args()
     return args
 
@@ -29,7 +34,8 @@ def get_dataset(dataset_name, K_shots):
 
 def main(args):
     dataset = get_dataset(args.dataset, args.K)
-    finn = MAML(args.model_type,
+    finn = MAML(dataset,
+                args.model_type,
                 args.loss_type,
                 dataset.dim_input,
                 dataset.dim_output,
@@ -39,7 +45,7 @@ def main(args):
                 args.batch_size
                 )
     finn.learn(args.batch_size, dataset, args.max_steps)
-
+    finn.test(dataset, args.test_sample)
 
 if __name__ == '__main__':
     args = argsparser()
--- maml.py
+++ maml.py
@@ -8,15 +8,23 @@ from loginfo import log
 
 # global variables for MAML
 LOG_FREQ = 100
+SAVE_FREQ = 1000
 
 
 class MAML(object):
-    def __init__(self, model_type, loss_type, dim_input, dim_output, alpha, beta, K, batch_size):
+    def __init__(self, dataset, model_type, loss_type, dim_input, dim_output, alpha, beta, K, batch_size):
         '''
-        alpha: fixed learning rate to calculate the gradient
-        beta: learning rate used for Adam Optimizer
+        model_tpye: choose model tpye for each task, choice: ('fc',)
+        loss_type:  choose the form of the objective function
+        dim_input:  input dimension
+        dim_output: desired output dimension
+        alpha:      fixed learning rate to calculate the gradient
+        beta:       learning rate used for Adam Optimizer
+        K:          perform K-shot learning
+        batch_size: number of tasks sampled in each iteration
         '''
         self.sess = utils.get_session(1)
+        self.dataset = dataset
         self.alpha = alpha
         self.K = K
         self.dim_input = dim_input
@@ -24,6 +32,7 @@ class MAML(object):
         self.batch_size = batch_size
         self.meta_optimizer = tf.train.AdamOptimizer(beta)
         self.avoid_second_derivative = False
+        self.task_name = "MAML.{}_{}-shot_{}-batch".format(dataset.name, self.K, self.batch_size)
         # Build placeholder
         self.build_placeholder()
         # Build model
@@ -34,10 +43,14 @@ class MAML(object):
         self.loss_fn = self.get_loss_fn(loss_type)
         self.build_graph(dim_input, dim_output, batch_norm=True)
         # Misc
-        summary_dir = 'log'
-        if not os.path.exists(summary_dir):
-            os.makedirs(summary_dir)
-        self.writer = tf.summary.FileWriter(summary_dir, self.sess.graph)
+        self.summary_dir = 'log'
+        if not os.path.exists(self.summary_dir):
+            os.makedirs(self.summary_dir)
+        self.writer = tf.summary.FileWriter(self.summary_dir, self.sess.graph)
+        self.checkpoint_dir = 'checkpoint'
+        if not os.path.exists(self.checkpoint_dir):
+            os.makedirs(self.checkpoint_dir)
+        self.saver = tf.train.Saver(max_to_keep=10)
 
     def build_placeholder(self):
         self.meta_train_x = tf.placeholder(tf.float32)
@@ -63,13 +76,14 @@ class MAML(object):
 
         self.weights = self.construct_weights(dim_input, dim_output)
 
-        # Do one meta step on 1 task
+        # Calculate loss on 1 task
         def metastep_graph(inp):
             meta_train_x, meta_train_y, meta_val_x, meta_val_y = inp
             weights = self.weights
             meta_train_output = self.contruct_forward(meta_train_x, weights,
                                                       reuse=False,
                                                       batch_norm=batch_norm)
+            # Meta train loss
             meta_train_loss = self.loss_fn(meta_train_y, meta_train_output)
             meta_train_loss = tf.reduce_mean(meta_train_loss)
             grads = dict(zip(weights.keys(),
@@ -82,7 +96,7 @@ class MAML(object):
             meta_val_output = self.contruct_forward(meta_val_x, new_weights,
                                                     reuse=True,
                                                     batch_norm=batch_norm)
-            # Meta loss
+            # Meta val loss
             meta_val_loss = self.loss_fn(meta_val_y, meta_val_output)
             meta_val_loss = tf.reduce_mean(meta_val_loss)
 
@@ -115,8 +129,12 @@ class MAML(object):
         for step in range(int(max_steps)):
             meta_val_loss, meta_train_loss = self.single_train_step(dataset, batch_size, step)
             if step % LOG_FREQ == 0:
-                log.infov("Meta train loss: {}, Meta val loss: {}".format(
+                log.infov("Meta train loss: {:.4f}, Meta val loss: {:.4f}".format(
                     meta_train_loss, meta_val_loss))
+            if step % SAVE_FREQ == 0:
+                log.infov("Save checkpoint-{}".format(step))
+                self.saver.save(self.sess, os.path.join(self.checkpoint_dir, self.task_name),
+                                global_step=step)
 
     def single_train_step(self, dataset, batch_size, step):
         batch_input, batch_target = dataset.get_batch(batch_size, resample=True)
@@ -130,3 +148,6 @@ class MAML(object):
                           feed_dict)
         self.writer.add_summary(summary_str, step)
         return meta_val_loss, meta_train_loss
+
+    def test(self, dataset, max_steps):
+        ipdb.set_trace()
